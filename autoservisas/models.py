57c3f3a23cf761f4caabdbc0558bcadd0ade4a41
Laimonas <laimonas.mituzas@gmail.com>
2026-01-28 08:27:12 +0200

    Models changed

--- autoservisas/models.py
+++ autoservisas/models.py
@@ -8,26 +8,27 @@ class Car(models.Model):
     client_name = models.CharField(verbose_name="Klientas", max_length=100)
 
     def __str__(self):
-        return self.license_plate
+        return f"{self.license_plate} - {self.model} - {self.client_name}"
+
 
 class Order(models.Model):
-    date = models.DateField(verbose_name="Data", max_length=10)
+    date = models.DateTimeField(verbose_name="Data", auto_now_add=True, max_length=10)
     car_id = models.ForeignKey(to="Car", verbose_name="Auto", on_delete=models.SET_NULL, null=True )
 
     def __str__(self):
-        return self.date
+        return f"{self.car} ({self.date})"
 
 class OrderLine(models.Model):
-    order_id = models.ForeignKey(to="Order", verbose_name="Uzsakymas", on_delete=models.SET_NULL, null=True )
+    order_id = models.ForeignKey(to="Order", verbose_name="Uzsakymas", on_delete=models.CASCADE, null=True )
     service_id = models.ForeignKey(to="Service", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True )
     quantity = models.IntegerField(verbose_name="Kiekis")
 
     def __str__(self):
-        return self.order_id
+        return f"{self.service.name} ({self.service.price}) - {self.quantity}"
 
 class Service(models.Model):
     name = models.CharField(verbose_name="Paslauga", max_length=100)
-    price = models.FloatField(verbose_name="Kaina", max_length=100)
+    price = models.DecimalField(verbose_name="Kaina", max_length=100, decimal_places=2, max_digits=10)
 
     def __str__(self):
         return self.name
