0e0ea29e83e9fe63411a34eb0cfc14f2810bd710
Laimonas <laimonas.mituzas@gmail.com>
2026-01-28 23:19:03 +0200

    Admin views added

--- autoservisas/admin.py
+++ autoservisas/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from .models import Car, Order, OrderLine, Service
 
+
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['car','date']
+
+    inlines = [OrderLineInLine]
+
+class OrderLineAdmin(admin.ModelAdmin):
+    list_display = ['order','service','quantity']
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['model','client_name','license_plate','vin_code']
+    list_filter = ['client_name','make','model']
+    search_fields = ['license_plate','vin_code']
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name','price']
+
 # Register your models here.
-admin.site.register(Car)
-admin.site.register(Order)
-admin.site.register(OrderLine)
-admin.site.register(Service)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLine, OrderLineAdmin)
+admin.site.register(Service, ServiceAdmin)
 
 
--- autoservisas/models.py
+++ autoservisas/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Car(models.Model):
-    make = models.CharField(verbose_name="Markė", max_length=100)
+    make = models.CharField(verbose_name = "Markė", max_length=100)
     model = models.CharField(verbose_name="Modelis", max_length=100)
     license_plate = models.CharField(verbose_name="Numeris", max_length=100)
     vin_code = models.CharField(verbose_name="VIN", max_length=100)
     client_name = models.CharField(verbose_name="Klientas", max_length=100)
+
+    class Meta:
+        verbose_name = "Car",
+        verbose_name_plural = "Cars"
 
     def __str__(self):
         return f"{self.license_plate} - {self.model} - {self.client_name}"
@@ -13,15 +17,24 @@ class Car(models.Model):
 
 class Order(models.Model):
     date = models.DateTimeField(verbose_name="Data", auto_now_add=True, max_length=10)
-    car_id = models.ForeignKey(to="Car", verbose_name="Auto", on_delete=models.SET_NULL, null=True )
+    car = models.ForeignKey(to="Car", verbose_name="Auto", on_delete=models.SET_NULL, null=True, blank=True )
+
+    class Meta:
+        verbose_name = "Order",
+        verbose_name_plural = "Orders"
 
     def __str__(self):
-        return f"{self.car} ({self.date})"
+        return f"{self.car}: ({self.date})"
 
+# Remonto darbu modelis
 class OrderLine(models.Model):
-    order_id = models.ForeignKey(to="Order", verbose_name="Uzsakymas", on_delete=models.CASCADE, null=True )
-    service_id = models.ForeignKey(to="Service", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True )
+    order = models.ForeignKey(to="Order", verbose_name="Uzsakymas", on_delete=models.CASCADE, null=True )
+    service = models.ForeignKey(to="Service", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True )
     quantity = models.IntegerField(verbose_name="Kiekis")
+
+    class Meta:
+        verbose_name = "Order Line",
+        verbose_name_plural = "Oder Lines"
 
     def __str__(self):
         return f"{self.service.name} ({self.service.price}) - {self.quantity}"
@@ -29,6 +42,11 @@ class OrderLine(models.Model):
 class Service(models.Model):
     name = models.CharField(verbose_name="Paslauga", max_length=100)
     price = models.DecimalField(verbose_name="Kaina", max_length=100, decimal_places=2, max_digits=10)
+
+    class Meta:
+        verbose_name = "Service",
+        verbose_name_plural = "Services"
+
 
     def __str__(self):
         return self.name
